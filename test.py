8f64a01548934609d8c4ef0a036169276c9197a5
erik_rohkohl <erikrohkohl@googlemail.com>
2017-07-29 14:06:48 +0200

    #1: added test for Kripke Structure init

--- test.py
+++ test.py
@@ -1,5 +1,17 @@
 from src.formula.atom import Atom
+from src.kripke.structure import KripkeStructure
+
 
 def test_atom_init():
     atom = Atom("p");
     assert atom.name == "p"
+
+
+def test_kripke_structure_init():
+    assign = {("p", True)}
+    worlds = {1}
+    relations = {}
+    ks = KripkeStructure(worlds, relations, assign)
+    assert ks.relations == {}
+    assert ks.worlds == {1}
+    assert ks.assignments == {("p", True)}
