6510cde8541fe1ee0ac94a904a26fa3939fdde1f
erik_rohkohl <erikrohkohl@googlemail.com>
2017-08-01 11:38:47 +0200

    #11: Box_a formula and semantics, DONE

--- src/formula.py
+++ src/formula.py
@@ -6,7 +6,7 @@ class Atom():
     def semantic(self, ks, world_to_test):
         for world in ks.worlds:
             if world.name == world_to_test:
-                return world.assignment[self.name]
+                return world.assignment.get(self.name, False)
 
 
 # Describes box operator of modal logic formula and it's semantics
@@ -17,6 +17,21 @@ class Box():
     def semantic(self, ks, world_to_test):
         result = True
         for relation in ks.relations:
+            if relation[0] == world_to_test:
+                result = result and self.mlp.semantic(ks, relation[1])
+
+        return result
+
+
+# Describes box operator of modal logic formula and it's semantics for Agent a
+class Box_a():
+    def __init__(self, agent, mlp):
+        self.mlp = mlp
+        self.agent = agent
+
+    def semantic(self, ks, world_to_test):
+        result = True
+        for relation in ks.relations.get(self.agent, {}):
             if relation[0] == world_to_test:
                 result = result and self.mlp.semantic(ks, relation[1])
 
---
+++ test/test_box_a.py
@@ -0,0 +1,35 @@
+from src.formula import Atom, Box_a
+from src.kripke import KripkeStructure, World
+
+
+def test_semantic_box_a_true():
+    worlds = [
+        World('1', {'p': False}),
+        World('2', {'p': True})]
+
+    relations = {'a': {('1', '2')}}
+    ks = KripkeStructure(worlds, relations)
+    mpl = Box_a('a', Atom('p'))
+    assert True == mpl.semantic(ks, '1')
+
+
+def test_semantic_box_a_false():
+    worlds = [
+        World('1', {'p': False}),
+        World('2', {'p': False})]
+
+    relations = {'a': {('1', '2')}}
+    ks = KripkeStructure(worlds, relations)
+    mpl = Box_a('a', Atom('p'))
+    assert False == mpl.semantic(ks, '1')
+
+
+def test_semantic_box_a_two_agents():
+    worlds = [
+        World('1', {'p': False}),
+        World('2', {'p': False})]
+
+    relations = {'a': {('1', '2')}, 'b': {}}
+    ks = KripkeStructure(worlds, relations)
+    mpl = Box_a('b', Atom('p'))
+    assert True == mpl.semantic(ks, '1')
