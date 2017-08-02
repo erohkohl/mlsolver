1547c3bfb6e6b394106e0e56f6c59bd9391bcfe3
erik_rohkohl <erikrohkohl@googlemail.com>
2017-08-02 10:38:22 +0200

    #12: DONE, changed color of node to grey

--- scripts/plot.py
+++ scripts/plot.py
@@ -6,29 +6,33 @@ from src.model import WiseMenWithHat
 
 # Routine to plot Kripke Frame with relations for all agents
 def draw_graph(ks, labels):
-    nodes = set(world.name for world in ks.worlds)
+    nodes = list(world.name for world in ks.worlds)
     G = nx.Graph()
 
     for node in nodes:
         G.add_node(node)
 
     label = {}
+    edge_list = []
     for agent, l in zip(ks.relations, labels):
         edges = ks.relations.get(agent)
         for edge in edges:
-            G.add_edge(edge[0], edge[1])
+            # G.add_edge(edge[0], edge[1])
+            edge_list.append((edge[0], edge[1]))
             label.update({edge: l})
 
     pos = nx.shell_layout(G)
-    nx.draw(G, pos)
-    nx.draw_networkx_nodes(G, pos, node_size=3000, node_color="white", linewidths=3.0)
+    nx.draw_networkx_nodes(G, pos, node_size=3000, node_color="lightgrey", linewidths=3.0)
+    nx.draw_networkx_edges(G, pos, edgelist=edge_list, width=2, alpha=0.5, edge_color='black')
 
     # labels
     nx.draw_networkx_labels(G, pos, font_size=15, font_family='sans-serif', label_pos=0.1)
     nx.draw_networkx_edge_labels(G, pos, label)
 
+    plt.axis('off')
+
 
 ks = WiseMenWithHat().ks
 plt.savefig("./scripts/wise_men_with_hat_graph.png")
-draw_graph(ks, ["Wise man 1", "Wise man 2", " Wise man 3"])
+draw_graph(ks, ["1", "2", "3"])
 plt.show()
